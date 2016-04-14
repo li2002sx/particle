b659efd1a73a411b2e864731deb6d176a98c788d
jankie <li2002sx@163.com>
2016-04-14 22:37:24 +0800

    需求开发

--- src/main/java/com/htche/particle/util/StringHelper.java
+++ src/main/java/com/htche/particle/util/StringHelper.java
@@ -83,4 +83,19 @@ public class StringHelper {
         }
         return str;
     }
+
+    public static String html(String content) {
+        if (content == null) return "";
+        String html = content;
+
+        //    html = html.replace( "'", "&apos;");
+        html = html.replaceAll("&", "&amp;");
+        html = html.replace("\"", "&quot;");  //"
+        html = html.replace("\t", "&nbsp;&nbsp;");// 替换跳格
+        html = html.replace(" ", "&nbsp;");// 替换空格
+        html = html.replace("<", "&lt;");
+        html = html.replaceAll(">", "&gt;");
+
+        return html;
+    }
 }
